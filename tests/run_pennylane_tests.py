e38ffe7e6590b2905c526b05f903164f962ea3c2
Alec Edgington <alec.edgington@cambridgequantum.com>
2022-09-05 09:47:23 +0100

    Fix some mypy issues.

--- tests/run_pennylane_tests.py
+++ tests/run_pennylane_tests.py
@@ -1,6 +1,6 @@
 # run battery of pennylane tests with default backend
 
 
-from pennylane.devices.tests import test_device
+from pennylane.devices.tests import test_device  # type: ignore
 
 test_device("pytket.pytketdevice", shots=None, pytest_args=["-x", "-s"])
