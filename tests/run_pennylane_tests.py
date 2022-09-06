6f6fbceeba7e3c0758178479a097b421db14d044
Alec Edgington <alec.edgington@cambridgequantum.com>
2022-09-06 11:45:34 +0100

    Fix parameter passing.

--- tests/run_pennylane_tests.py
+++ tests/run_pennylane_tests.py
@@ -8,6 +8,6 @@ pytest_args = ["-x", "-s"]
 if platform.system() == "Darwin":
     # TODO Remove this exclusion.
     # https://github.com/CQCL/pytket-pennylane/issues/2
-    pytest_args.extend(["-k", "not", "test_supported_gate_two_wires_with_parameters"])
+    pytest_args.extend(["-k", "not test_supported_gate_two_wires_with_parameters"])
 
 test_device("pytket.pytketdevice", shots=None, pytest_args=pytest_args)
