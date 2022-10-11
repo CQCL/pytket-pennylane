7cd1a2e44ac7cd0ed6e6c1e13b24af4891e8b509
melf <cqc@melf.de>
2022-10-11 11:14:24 +0100

    clean up from other extensions and version updates

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
     install_requires=[
-        "pytket ~= 1.5",
+        "pytket ~= 1.7",
         "pennylane ~= 0.25.0",
         "pytket-qiskit ~= 0.28.0",
     ],
