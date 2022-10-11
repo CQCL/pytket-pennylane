1778350ed630287d6042bb3ad5ac9707315c5447
Alec Edgington <alec.edgington@cambridgequantum.com>
2022-10-11 10:45:34 +0100

    Restore test on MacOS.

--- tests/run_pennylane_tests.py
+++ tests/run_pennylane_tests.py
@@ -5,9 +5,9 @@ from pennylane.devices.tests import test_device  # type: ignore
 
 pytest_args = ["-x", "-s"]
 
-if platform.system() == "Darwin":
-    # TODO Remove this exclusion.
-    # https://github.com/CQCL/pytket-pennylane/issues/2
-    pytest_args.extend(["-k", "not test_supported_gate_two_wires_with_parameters"])
+# if platform.system() == "Darwin":
+#     # TODO Remove this exclusion.
+#     # https://github.com/CQCL/pytket-pennylane/issues/2
+#     pytest_args.extend(["-k", "not test_supported_gate_two_wires_with_parameters"])
 
 test_device("pytket.pytketdevice", shots=None, pytest_args=pytest_args)
