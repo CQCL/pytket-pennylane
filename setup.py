0413bb68c37de2e5a7dfb6d0500d0892f552b996
Alec Edgington <alec.edgington@cambridgequantum.com>
2022-12-19 10:01:12 +0000

    Update to pytket-qiskit 0.32.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     install_requires=[
         "pytket ~= 1.9",
         "pennylane ~= 0.27.0",
-        "pytket-qiskit ~= 0.28.0",
+        "pytket-qiskit ~= 0.32.0",
     ],
     classifiers=[
         "Environment :: Console",
