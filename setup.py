44fe7b68c9354ae7c4bdafd70d48f92b039532f1
Alec Edgington <alec.edgington@cambridgequantum.com>
2023-01-12 08:55:10 +0000

    Update pennylane to 0.28.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "pytket ~= 1.10",
-        "pennylane ~= 0.27.0",
+        "pennylane ~= 0.28.0",
         "pytket-qiskit ~= 0.32.0",
     ],
     classifiers=[
