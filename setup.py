8413928d107ac3a40752cc2e8dfbf8f070040d74
Alec Edgington <alec.edgington@quantinuum.com>
2023-05-05 11:25:58 +0100

    Update to pennylane 0.30.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "pytket ~= 1.11",
-        "pennylane ~= 0.28.0",
+        "pennylane ~= 0.30.0",
         "pytket-qiskit ~= 0.32.0",
     ],
     classifiers=[
