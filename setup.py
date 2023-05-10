e5545b633210e06b356bf45dee82425937604505
Melf <cqc@melf.de>
2023-05-10 11:08:17 +0100

    update pytket verion

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
     install_requires=[
-        "pytket ~= 1.14",
+        "pytket == 1.15.0rc1",
         "pennylane ~= 0.30.0",
         "pytket-qiskit ~= 0.32.0",
     ],
