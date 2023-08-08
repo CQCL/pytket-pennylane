8132aed9777d9b780c100fc65e15d5507510aeac
Melf <cqc@melf.de>
2023-08-08 14:48:40 +0100

    update version, changelog and pytket

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
     install_requires=[
-        "pytket ~= 1.17",
+        "pytket ~= 1.18",
         "pennylane ~= 0.31.0",
         "pytket-qiskit ~= 0.41.0",
     ],
