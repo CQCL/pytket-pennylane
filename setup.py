d514f40e010f9363b60897927d0245a100d7f5b0
Silas Dilkes <36165522+sjdilkes@users.noreply.github.com>
2024-01-12 12:38:30 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     version=metadata["__extension_version__"],
     author="KN",
     author_email="seyon.sivarajah@cambridgequantum.com",
-    python_requires=">=3.9",
+    python_requires=">=3.10",
     project_urls={
         "Documentation": "https://tket.quantinuum.com/extensions/pytket-pennylane/index.html",
         "Source": "https://github.com/CQCL/pytket-pennylane",
@@ -51,9 +51,9 @@ setup(
     ],
     classifiers=[
         "Environment :: Console",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: Other/Proprietary License",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX :: Linux",
