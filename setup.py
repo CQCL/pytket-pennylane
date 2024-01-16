82a2fdf929a538bd663ed55aa2f948e857f79a45
Silas Dilkes <36165522+sjdilkes@users.noreply.github.com>
2024-01-16 14:46:28 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
     install_requires=[
-        "pytket ~= 1.24.0rc0",
+        "pytket ~= 1.24",
         "pennylane ~= 0.34.0",
         "pytket-qiskit >= 0.46,< 0.48",
     ],
