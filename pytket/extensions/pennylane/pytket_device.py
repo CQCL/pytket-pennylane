879a575fb3d501899b2191e724b5a286d5e17002
Alec Edgington <alec.edgington@quantinuum.com>
2025-01-23 14:41:59 +0000

    Update dependencies.

--- pytket/extensions/pennylane/pytket_device.py
+++ pytket/extensions/pennylane/pytket_device.py
@@ -36,7 +36,7 @@ class PytketDevice(QubitDevice):
 
     name = "pytket-pennylane plugin"
     short_name = "pytket.pytketdevice"
-    pennylane_requires = ">=0.35.0"
+    pennylane_requires = ">=0.40.0"
     version = "0.15.0"
     plugin_version = __extension_version__
     author = "KN"
