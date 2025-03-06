ace7b569f8b36969576b0dc656ce3260ce99a157
cqc-melf <70640934+cqc-melf@users.noreply.github.com>
2025-03-06 08:42:42 +0000

    update pytket version to 2.x (#141)
    * update pytket version to 2.x
    
    * update changelog

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
     install_requires=[
-        "pytket >= 1.39.0",
+        "pytket >= 2.0.1",
         "pennylane >= 0.40.0",
         "pytket-qiskit >= 0.62.0",
     ],
